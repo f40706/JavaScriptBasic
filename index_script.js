e4f986d42212e901f054387c304d94be23fda1ed
DESKTOP-M451O6I\John <f40706@gmail.com>
2022-09-24 15:54:41 +0800

    add code

---
+++ index_script.js
@@ -0,0 +1,85 @@
+/* const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+alert(century); */
+/* console.log(typeof true)
+let data = 123
+console.log(typeof data)
+data = '34A'
+console.log(typeof data)
+let number;
+console.log(typeof number) */
+// const year = 1991;
+// year = 1990; //const不能夠中途變更參數
+
+/* var month = 12;
+var month = 11;
+console.log(month);
+
+let day = 20;
+let day = 23;
+console.log(day); */
+
+// work = '123'
+// console.log(work)
+
+// let number = 11 % 2;
+// console.log(number);
+
+/* let x;
+let y;
+let z;
+z = x = y = 10 + 20 + 30;
+console.log(x, y, z); */
+
+// let name1 = 'apple';
+// console.log(`string = ${name1}`);
+
+// let string = "1234\n\
+// 2345\n\
+// 3456\n";
+// console.log(string);
+
+// let data = -2;
+// console.log(!data);
+
+// const year = '1991'
+// console.log(typeof Number(year))
+// console.log(Number(year))
+// console.log(typeof year)
+
+// const fileName = "123.mov"
+// console.log(Number(fileName))
+// console.log(typeof NaN)
+
+// if (isNaN(Number(fileName))) {
+//     console.log("is NaN")
+// }
+
+// // 0, undefined, null, NaN, ''
+// console.log(Boolean(0))
+// console.log(Boolean(undefined))
+// console.log(Boolean(null))
+// console.log(Boolean(NaN))
+// console.log(Boolean(''))
+
+const age = '18'
+if (age == 18) {
+    console.log("OK1")
+}
+if (age === 18) {
+    console.log("OK2")
+}
+
+if (age != 18) {
+    console.log("! OK1")
+}
+if (age !== 18) {
+    console.log("! OK2")
+}
